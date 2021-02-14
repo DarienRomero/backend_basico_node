d308ad39da1ca1b1359723a4096917ac27df97f2
Darien Romero <darien.romero@pucp.edu.pe>
2021-02-14 16:20:18 -0500

    Usuarios CRUD listo

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,36 +1,65 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require("../models/usuario");
 
-const usuariosGet = (req = request, res = response) => {
-    const { nombre = "No name", apellido, apikey, page = 1, limit } = req.query;
+const usuariosGet = async(req = request, res = response) => {
+    // const { nombre = "No name", apellido, apikey, page = 1, limit } = req.query;
     // res.send('Hello World') Para enviar un sitio web
-    res.json({
-        msg: "Get al api - controlador",
-        nombre,
-        apellido,
-        apikey,
-        page,
-        limit
-    });
-};
-const usuariosPut = (req, res = response) => {
-    const { id } = req.params;
-    res.json({
-        msg: "put al api",
-        id
-    });
-};
-const usuariosPost = (req, res = response) => {
-    const body = req.body;
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
+    /* total = await Usuario.countDocuments(query);
+    usuarios = await Usuario.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite)); */
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite))
+    ]);
 
     res.json({
-        msg: "post al api",
-        body
+        total,
+        usuarios
     });
 };
-const usuariosDelete = (req, res = response) => {
+const usuariosPut = async(req, res = response) => {
+    const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
+    //TODO: Validar contra base de datos
+    if (password) {
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
     res.json({
-        msg: "delete al api"
+        usuario
     });
+};
+const usuariosPost = async(req, res = response) => {
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({
+        nombre,
+        correo,
+        password,
+        rol
+    });
+    //Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+    //Guardar en DB
+    await usuario.save();
+
+    res.json({
+        usuario
+    });
+};
+const usuariosDelete = async(req, res = response) => {
+    const { id } = req.params;
+    // const usuario = await Usuario.findByIdAndDelete(id);
+    const usuario = await Usuario.findByIdAndUpdate(id, { estado: false });
+    res.json(usuario);
 };
 module.exports = {
     usuariosGet,
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,13 +1,30 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete } = require('../controllers/usuarios');
-
+const { esRoleValido, existeEmail, existeUsuarioPorID } = require('../helpers/db-validators');
 const router = Router();
 
 router.get('/', usuariosGet);
-router.put('/:id', usuariosPut);
-router.post('/', usuariosPost);
-router.delete('/', usuariosDelete);
-
-
+router.put('/:id', [
+    check('id', 'No es un id válido').isMongoId(),
+    check('id').custom((id) => existeUsuarioPorID(id)),
+    // check('rol').custom((rol) => esRoleValido(rol)),
+    validarCampos
+], usuariosPut);
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio y más de 6 letras').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    // check('rol', 'No es un rol válido').isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    check('correo').custom((correo) => existeEmail(correo)),
+    check('rol').custom((rol) => esRoleValido(rol)),
+    validarCampos
+], usuariosPost);
+router.delete('/:id', [
+    check('id', 'No es un id válido').isMongoId(),
+    check('id').custom((id) => existeUsuarioPorID(id)),
+    validarCampos
+], usuariosDelete);
 
 module.exports = router;
